2477f82cdba2807a940572f408dab593d1411494
NuclearW <incongruency@gmail.com>
2012-05-09 22:34:24 -0400

    Start commands

--- src/main/java/com/nuclearw/postoffice/PostOfficeCommandExecutor.java
+++ src/main/java/com/nuclearw/postoffice/PostOfficeCommandExecutor.java
@@ -5,6 +5,12 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class PostOfficeCommandExecutor implements CommandExecutor {
+	private static PostOffice plugin;
+
+	public PostOfficeCommandExecutor(PostOffice plugin) {
+		this.plugin = plugin;
+	}
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		// TODO Auto-generated method stub
