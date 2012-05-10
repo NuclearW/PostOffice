14e66f60eaf8c5415c8e79ccdcf32fbcca92dc58
NuclearW <incongruency@gmail.com>
2012-05-09 22:59:07 -0400

    Some work on the PostMaster

--- src/main/java/com/nuclearw/postoffice/PostMaster.java
+++ src/main/java/com/nuclearw/postoffice/PostMaster.java
@@ -1,18 +1,44 @@
 package com.nuclearw.postoffice;
 
-import org.bukkit.Location;
+import java.io.File;
+
+import com.nuclearw.postoffice.mail.Mail;
 
 public class PostMaster {
 	private static PostMaster instance;
 	private static PostOffice plugin;
 
+	private static File dataDir;
+
 	private PostMaster(PostOffice plugin) {
 		this.plugin = plugin;
+
+		File rootDir = plugin.getDataFolder();
+		this.dataDir = new File(rootDir, "boxes");
 	}
 
-	public static PostMaster getInstance(PostOffice plugin) {
+	/**
+	 * Get or create the instance of PostMaster
+	 *
+	 * @param plugin The PostOffice plugin instance
+	 * @return The PostMaster instance
+	 */
+	protected static PostMaster getInstance(PostOffice plugin) {
 		if(instance != null) return instance;
 		instance = new PostMaster(plugin);
 		return instance;
+	}
+
+	/**
+	 * Get the instance of PostMaster
+	 *
+	 * @return The PostMaster instance, or null if it has not been made yet
+	 */
+	public static PostMaster getInstance() {
+		return instance;
+	}
+
+	public static void sendMail(Mail mail) {
+		// TODO: Handle mail
 	}
 }
