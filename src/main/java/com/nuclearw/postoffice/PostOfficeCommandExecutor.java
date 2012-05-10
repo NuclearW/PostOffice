18c901bdb95600a455fa8a2e6044e7f80c64da4c
NuclearW <incongruency@gmail.com>
2012-05-10 00:30:09 -0400

    Cancel mail

--- src/main/java/com/nuclearw/postoffice/PostOfficeCommandExecutor.java
+++ src/main/java/com/nuclearw/postoffice/PostOfficeCommandExecutor.java
@@ -3,6 +3,7 @@ package com.nuclearw.postoffice;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PostOfficeCommandExecutor implements CommandExecutor {
 	private static PostOffice plugin;
@@ -13,8 +14,33 @@ public class PostOfficeCommandExecutor implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		if(!label.equalsIgnoreCase("mail")) return false;
 
+		if(args.length == 1) {
+			if(args[0].equalsIgnoreCase("cancel")) {
+				if(!(sender instanceof Player)) {
+					sender.sendMessage("Console cannot cancel mail.");
+				}
+
+				Player player = (Player) sender;
+
+				if(plugin.isHoldingMail(player)) {
+					if(PostMaster.returnMail(plugin.getHeldMail(player))) {
+						player.sendMessage("Mail returned.");
+					} else {
+						player.sendMessage("Mail could not be returned, mail lost.");
+					}
+					plugin.removeHeldMail(player);
+				} else {
+					player.sendMessage("You aren't holding mail!");
+				}
+			} else {
+				return false;
+			}
+		} else if(args.length > 2) {
+			// TODO: Make mail to be sent
+		}
+
+		return true;
+	}
 }
