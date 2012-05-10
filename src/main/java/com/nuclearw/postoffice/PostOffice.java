240350d3b2699dc87e752c48ce40d5730e1b0e50
NuclearW <incongruency@gmail.com>
2012-05-09 23:43:54 -0400

    Held mail

--- src/main/java/com/nuclearw/postoffice/PostOffice.java
+++ src/main/java/com/nuclearw/postoffice/PostOffice.java
@@ -1,14 +1,17 @@
 package com.nuclearw.postoffice;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.persistence.PersistenceException;
-
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.nuclearw.postoffice.mail.Mail;
 
 public class PostOffice extends JavaPlugin {
 	private static PostMaster manager;
+
+	private static Map<Player, Mail> held = new HashMap<Player, Mail>();
 
 	@Override
 	public void onEnable() {
@@ -27,6 +30,39 @@ public class PostOffice extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
+		// Goodbye, world
 		getLogger().info("Finished Unloading "+getDescription().getFullName());
+	}
+
+	/**
+	 * Get if a player is currently holding mail
+	 *
+	 * @param player Player to check if holding mail
+	 * @return True if the player is holding mail currently, false if not
+	 */
+	public boolean isHoldingMail(Player player) {
+		return held.containsKey(player);
+	}
+
+	/**
+	 * Get held mail
+	 *
+	 * @param player Player to get held mail from
+	 * @return The mail the player is holding
+	 */
+	public Mail getHeldMail(Player player) {
+		return held.get(player);
+	}
+
+	/**
+	 * Give mail to a player to hold
+	 *
+	 * @param player Player to give mail to
+	 * @param mail Mail to hold
+	 * @return True if mail was given, false if not
+	 */
+	public boolean holdMail(Player player, Mail mail) {
+		if(isHoldingMail(player)) return false;
+		return (held.put(player, mail) != null);
 	}
 }
