58a2dd25d87e17995e741bc583f9f1cdc5a05c06
NuclearW <incongruency@gmail.com>
2012-05-11 00:16:45 -0400

    What can brown do for you?

--- src/main/java/com/nuclearw/postoffice/mail/Package.java
+++ src/main/java/com/nuclearw/postoffice/mail/Package.java
@@ -2,22 +2,24 @@ package com.nuclearw.postoffice.mail;
 
 import org.bukkit.inventory.ItemStack;
 
+import com.nuclearw.postoffice.util.CardboardBox;
+
 public class Package implements Mail {
 	private static final long serialVersionUID = 8606293052193623144L;
 
 	private final String sentTo, sentFrom;
-	private final ItemStack item;
+	private final CardboardBox item;
 	private final long sentAt;
 
 	public Package(String sentTo, String sentFrom, ItemStack item) {
 		this.sentTo = sentTo;
 		this.sentFrom = sentFrom;
-		this.item = item;
+		this.item = new CardboardBox(item);
 		this.sentAt = System.currentTimeMillis();
 	}
 
 	public ItemStack getItem() {
-		return item;
+		return item.unbox();
 	}
 
 	@Override
