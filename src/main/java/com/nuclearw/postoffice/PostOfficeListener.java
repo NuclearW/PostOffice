08ad44fa47c812cc83713101f32b844747d7af5a
NuclearW <incongruency@gmail.com>
2012-05-11 01:11:56 -0400

    Priority to HIGH

--- src/main/java/com/nuclearw/postoffice/PostOfficeListener.java
+++ src/main/java/com/nuclearw/postoffice/PostOfficeListener.java
@@ -11,6 +11,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -86,7 +87,7 @@ public class PostOfficeListener implements Listener {
 		*/
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onSignChange(SignChangeEvent event) {
 		String marker = event.getLine(0);
 
